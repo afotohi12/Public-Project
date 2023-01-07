06e02864aafded01e4a84c32b6f688a0b5b44012
Alireza <73761020+afotohi12@users.noreply.github.com>
2023-01-07 22:08:13 +0330

    Add files via upload

---
+++ Ticket AirPlane/js/script.js
@@ -0,0 +1,26 @@
+﻿
+
+let countrySelectBox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+
+let countriesData = {
+  Iran: ["تبریز", "تهران", "اردبیل", "خلخال", "گیلان"],
+  Turkey: ["آلانیا", "انتالیا", "ازمیر", "استانبول"],
+  US: ["لاس وگاس", "سان فرانسیسکو", "نیویورک"],
+};
+
+countrySelectBox.addEventListener("change",function(){
+  if(countrySelectBox.value === "انتخاب"){
+    citySelect.innerHTML = "";
+    citySelect.innerHTML += "<option>انتخاب</option>";
+  }else{
+    let mainCountryName = countrySelectBox.value;
+    let mainCountryCities = countriesData[mainCountryName];
+    citySelect.innerHTML = "";
+ 
+    mainCountryCities.forEach(function(city){
+      citySelect.innerHTML += "<option> "+city+ "</option>"
+    });
+
+  }
+});
